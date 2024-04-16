6227d989510bf7b3aa850c4769b7c6d34ce98057
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2024-04-16 18:39:47 +0300

    add urls endpoint register-list

--- events/urls.py
+++ events/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
 from events.apps import EventsConfig
-from events.views import EventListCreateAPIView, EventDetailAPIView, RegisterToEventAPIView, CancelRegistrationAPIView, \
-    index
+from events.views import EventListCreateAPIView, EventDetailAPIView, RegisterToEventAPIView, CancelRegistrationAPIView,\
+    RegisterListAPIView
 
 app_name = EventsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
     path('events/', EventListCreateAPIView.as_view(), name='event-list'),
     path('events/<int:pk>/', EventDetailAPIView.as_view(), name='event-detail'),
+    path('register/', RegisterListAPIView.as_view(), name='register-list'),
     path('<int:event_id>/register/', RegisterToEventAPIView.as_view(), name='register-to-event'),
-    path('<int:event_id>/cancel-registration/', CancelRegistrationAPIView.as_view(), name='cancel-registration'),
+    path('<int:event_id>/cancel-registration/<int:registration_id>/', CancelRegistrationAPIView.as_view(),
+         name='cancel-registration'),
 ]
