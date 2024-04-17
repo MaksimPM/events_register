49ebdce26b7ba116e9e86fffa93e7fd118ec3315
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2024-04-17 19:42:48 +0300

    fix redis for docker

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_TIMEZONE = "Europe/Moscow"
